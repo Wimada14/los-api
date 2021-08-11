648e6f1fbbfbdb2077c587ee09b7c11d27e205a6
wimada14 <wimada@efinancethai.com>
2021-08-11 21:42:10 +0700

    Add project files.

---
+++ Model/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace los_api.Model
+{
+    public class ProductModel
+    {
+        public String id { get; set; }
+        public String name { get; set; }
+        public String imageUrl { get; set; }
+        public String price { get; set; }
+    }
+}
---
+++ Model/StockModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace los_api.Model
+{
+    public class StockModel
+    {
+        public String id { get; set; }
+        public String productId { get; set; }
+        public int amount { get; set; }
+    }
+}
